2839aad2ad7194a1274945d989cfa6214646fdc1
Lukas <lerni@users.noreply.github.com>
2022-12-02 09:02:25 +0100

    Hamaka\Tasks namespace
    `Hamaka\Tasks` namespace to reflect class name used for config in README and also actually used directory.

--- src/Tasks/UserFormsCleanupOldEntriesTask.php
+++ src/Tasks/UserFormsCleanupOldEntriesTask.php
@@ -1,47 +1,47 @@
 <?php
 
-    namespace Hamaka;
+namespace Hamaka\Tasks;
 
-    use SilverStripe\Dev\BuildTask;
-    use SilverStripe\ORM\DB;
-    use SilverStripe\UserForms\Model\Submission\SubmittedForm;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\ORM\DB;
+use SilverStripe\UserForms\Model\Submission\SubmittedForm;
 
-    class UserFormsCleanupOldEntriesTask extends BuildTask
+class UserFormsCleanupOldEntriesTask extends BuildTask
+{
+
+    protected $title = "UserForms Clean-up SubmittedForm task";
+
+    protected $description = "Removes old userdata for privacy reasons";
+
+    private static $segment = 'userforms-cleanup';
+
+    private static $days_retention = 31;
+
+    public function run($request)
     {
+        $iThresholdDate = strtotime('-' . $this->config()->get('days_retention') . ' days');
+        $sThresholdDate = date('Y-m-d 00:00:00', $iThresholdDate);
+        DB::alteration_message('Removing all entries before ' . $sThresholdDate);
 
-        protected $title = "UserForms Clean-up SubmittedForm task";
+        DB::alteration_message('Total entries in database (before cleanup): ' . SubmittedForm::get()->count());
 
-        protected $description = "Removes old userdata for privacy reasons";
+        $iClearedEntries = $this->cleanUpUserForms($sThresholdDate);
+        DB::alteration_message('Total entries to be deleted: ' . $iClearedEntries);
 
-        private static $segment = 'userforms-cleanup';
-
-        private static $days_retention = 31;
-
-        public function run($request)
-        {
-            $iThresholdDate = strtotime('-' . $this->config()->get('days_retention') . ' days');
-            $sThresholdDate = date('Y-m-d 00:00:00', $iThresholdDate);
-            DB::alteration_message('Removing all entries before ' . $sThresholdDate);
-
-            DB::alteration_message('Total entries in database (before cleanup): ' . SubmittedForm::get()->count());
-
-            $iClearedEntries = $this->cleanUpUserForms($sThresholdDate);
-            DB::alteration_message('Total entries to be deleted: ' . $iClearedEntries);
-
-            DB::alteration_message("Done, total entries after cleanup: " . SubmittedForm::get()->count());
-        }
-
-        /**
-         * @param $sBeforeDate String Date written as Y-m-d h:m:s
-         *
-         * @return int
-         */
-        public static function cleanUpUserForms(string $sBeforeDate): int
-        {
-            $dlSubmissions     = SubmittedForm::get()->filter('Created:LessThanOrEqual', $sBeforeDate);
-            $iTotalToBeCleared = $dlSubmissions->count();
-            $dlSubmissions->removeAll();
-
-            return $iTotalToBeCleared;
-        }
+        DB::alteration_message("Done, total entries after cleanup: " . SubmittedForm::get()->count());
     }
+
+    /**
+     * @param $sBeforeDate String Date written as Y-m-d h:m:s
+     *
+     * @return int
+     */
+    public static function cleanUpUserForms(string $sBeforeDate): int
+    {
+        $dlSubmissions     = SubmittedForm::get()->filter('Created:LessThanOrEqual', $sBeforeDate);
+        $iTotalToBeCleared = $dlSubmissions->count();
+        $dlSubmissions->removeAll();
+
+        return $iTotalToBeCleared;
+    }
+}
